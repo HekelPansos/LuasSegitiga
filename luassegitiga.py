a865f455a6a3db68d07ef042b642abb26b890149
HekelPansos <69354104+HekelPansos@users.noreply.github.com>
2020-08-07 16:25:19 +0000

    Add files via upload

---
+++ luassegitiga.py
@@ -0,0 +1,25 @@
+#program mencari luas segitiga
+print
+print ("\033[0;34;5m===============================")
+print ("\033[0;35;5mAuthor : ./<Dark>[Viper]")
+print ("\033[0;35;5mIG.    : mr_viper22")
+print ("\033[0;35;5mWA.    : +62 838-6285-1420")
+print ("\033[0;35;5mGC.    : Dark Cyber Team")
+print ("\033[0;34;5m===============================")
+print
+print
+print ("\033[0;34;5m[+]=========================[+]")
+#alas segitiga
+a = float(input("\033[1;33;5mPanjang Alas Segitiga : "))
+print
+print ("\033[0;34;5m[+]=========================[+]")
+#tinggi segitiga
+t = float(input("\033[1;32;5mMasukkan Tinggi Segitiga : "))
+print
+print ("\033[0;34;5m[+]=========================[+]")
+#rumus segitiga
+luas = 0.5*a*t
+print
+#menampilkan jawaban luas segitiga
+print("\033[1;36;5mLuas Segitiga Adalah : "+str(luas))
+print ("\033[0;34;5m[+]=========================[+]")
